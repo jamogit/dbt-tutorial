86b0db007ba6f5c2978038a803556bd74b1d9eda
jan.moilanen@gmail.com <jan.moilanen@gmail.com>
2021-05-10 08:19:19 +0000

    staging directory

--- models/customers.sql
+++ models/customers.sql
@@ -1,3 +1,9 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
 with customers as (
 
     select * from {{ref('stg_customers')}}
@@ -6,13 +12,7 @@ with customers as (
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
